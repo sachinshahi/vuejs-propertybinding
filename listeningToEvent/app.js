4c8cf413595e513f873a4fd0b4d9c59d70660b2c
Sachin Shahi <sachinshahi.2010@gmail.com>
2020-01-19 20:51:44 +0545

    listening to event

---
+++ listeningToEvent/app.js
@@ -0,0 +1,10 @@
+// Put this in the script section in JSFiddle
+// In a local setup, you need to merge this and the index.html file into one file
+new Vue({
+	el: '#app',
+	data: {
+		counter: 0
+	},
+	methods: {
+	}
+})
